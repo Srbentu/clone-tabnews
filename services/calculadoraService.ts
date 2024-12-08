28432d5eaa4581add0d8545b4b90c2112f7d2643
Breno Marcomini <brenomarcomini@hotmail.com>
2024-12-08 17:33:44 -0300

    delete old file

--- services/calculadoraService.ts
+++
@@ -1,7 +0,0 @@
-import { Calculadora, Resultado } from "../types/calculadora";
-
-export class CalculadoraService {
-  somar({ arg1, arg2 }: Calculadora): Resultado {
-    return { resultado: arg1 + arg2 };
-  }
-}
