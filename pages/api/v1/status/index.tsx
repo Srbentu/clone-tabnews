9e745bb3213154e9759704429c5fb5d6ab3870c5
Breno Marcomini <brenomarcomini@hotmail.com>
2024-12-10 21:01:57 -0300

    database.js

--- pages/api/v1/status/index.tsx
+++ pages/api/v1/status/index.tsx
@@ -1,8 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import database from "../../../../infra/database";
 
-export default function statusApi(
+export default async function statusApi(
   request: NextApiRequest,
   response: NextApiResponse,
 ) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result);
   response.status(200).json({ status: "ok" });
 }
