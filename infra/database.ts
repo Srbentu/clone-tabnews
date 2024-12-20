cbf3ac2a54671eac39f5cc6faf0be453bfba9b71
Srbentu <srbentu@gmail.com>
2024-12-19 22:40:32 -0300

    ajustes

--- infra/database.ts
+++ infra/database.ts
@@ -7,6 +7,7 @@ async function query(queryObject: string | QueryConfig): Promise<QueryResult> {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: true,
   });
 
   try {
